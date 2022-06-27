259764cdf71ce7b858ab597bfabad9548bf4a00a
Alexey <alexkotser@gmail.com>
2022-06-27 21:10:33 +0300

    f

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
 // OMDB: `http://www.omdbapi.com/?apikey=f4f98778&s=${search}`
 // TMDB: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b69c1933d70772560f256dfcc45c6056`
-
-function searchMovies(id) {
-  localStorage.setItem("id", id)
-}
--- movies.js
+++ movies.js
@@ -3,29 +3,37 @@
 
 const movieEl = document.querySelector(".movie")
 const searchResultEl = document.querySelector(".search__result")
-const id = localStorage.getItem("id")
-
-function onSearchChange(event) {
-  const id = event.target.value
-  searchResultEl.innerHTML = event.target.value
-  renderMovies(id)
-}
 
 async function renderMovies(search) {
-  const movies = await fetch(`http://www.omdbapi.com/?apikey=f4f98778&s=${search}`)
+  const movies = await fetch(
+    `http://www.omdbapi.com/?apikey=f4f98778&s=${search}`
+  )
   const moviesData = await movies.json()
   console.log(moviesData)
-  movieEl.innerHTML = moviesData.Search
-    .map((search) => moviesHTML(search))
-    .join("").slice(0, 8)
+  movieEl.innerHTML = await moviesData.Search.map((search) =>
+    moviesHTML(search)
+  )
+    .slice(0, 8)
+    .join("")
+}
+
+async function onSearchChange(event) {
+  const searchValue = event.target.value
+  localStorage.setItem("searchValue", searchValue)
+  await renderMovies(searchValue)
+  searchResultEl.innerHTML = localStorage.getItem("searchValue")
+  console.log(searchValue)
 }
 
 function moviesHTML(search) {
   return `
-  <div class="movie">
-      <img src="${search.Poster}" class="movie__poster" alt="">
-    </div>
-    `
+    <div class="movie">
+        <img
+          src="${search.Poster}"
+          class="movie__poster"
+          alt=""
+        />
+    </div>`
 }
 
-renderMovies('fast')
+renderMovies("stranger things")
