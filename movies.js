8be26654a7659b63d2e1d615ab9172b24bb95a45
Alexey <alexkotser@gmail.com>
2022-06-27 14:59:46 +0300

    f

--- movies.js
+++ movies.js
@@ -1,3 +1,6 @@
+// OMDB: `http://www.omdbapi.com/?apikey=f4f98778&s=${search}`
+// TMDB: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b69c1933d70772560f256dfcc45c6056`
+
 const movieEl = document.querySelector(".movie")
 const searchResultEl = document.querySelector(".search__result")
 const id = localStorage.getItem("id")
@@ -9,18 +12,18 @@ function onSearchChange(event) {
 }
 
 async function renderMovies(search) {
-  const movies = await fetch(`https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=b69c1933d70772560f256dfcc45c6056&query=${search}`)
+  const movies = await fetch(`http://www.omdbapi.com/?apikey=f4f98778&s=${search}`)
   const moviesData = await movies.json()
   console.log(moviesData)
-  movieEl.innerHTML = moviesData.results
+  movieEl.innerHTML = moviesData
     .map((search) => moviesHTML(search))
     .join("")
 }
 
 function moviesHTML(search) {
   return `<div class="movie">
-      <img src="${search.posterPath}" class="movie__poster" alt="">
+      <img src="${search.poster}" class="movie__poster" alt="">
     </div>`
 }
 
-renderMovies()
+renderMovies('fast')
