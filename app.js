6ae5709e5964b73a6ff1317634b5f7b1cd4b7187
Alexey <alexkotser@gmail.com>
2022-06-28 20:30:54 +0300

    f

--- app.js
+++ app.js
@@ -4,26 +4,28 @@
 const moviesEl = document.querySelector(".movies")
 const searchResultEl = document.querySelector(".search__result")
 const formEl = document.querySelector(".browser--wrapper")
+// const loading = document.querySelector(".loading")
 
 async function renderMovies(search) {
   const movies = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=b69c1933d70772560f256dfcc45c6056&query=${search}`
   )
   const moviesData = await movies.json()
-  console.log(moviesData)
-//   const newMoviesData = []
-//   for (let i = 0; i < moviesData.length; ++i) {
-//     if (search.results.poster_path[i] !== null || search.results.poster_path[i] !== undefined) {
-//         newMoviesData.push(moviesData[i])
-//     }
-//   }
-//   console.log(newMoviesData)
+  // loading.classList += " visible"
+  
+  //   const newMoviesData = []
+  //   for (let i = 0; i < moviesData.length; ++i) {
+  //     if (search.results.poster_path[i] !== null || search.results.poster_path[i] !== undefined) {
+  //         newMoviesData.push(moviesData[i])
+  //     }
+  //   }
+  //   console.log(newMoviesData)
 
-    if(moviesData.total_results === 0) {
-      document.querySelector(".not__found").style.display = "flex"
-    } else {
-      document.querySelector(".not__found").style.display = "none"
-    }
+  if (moviesData.total_results === 0) {
+    document.querySelector(".not__found").style.display = "flex"
+  } else {
+    document.querySelector(".not__found").style.display = "none"
+  }
   moviesEl.innerHTML = /*new*/moviesData.results
     .map((search) => moviesHTML(search))
     .slice(0, 8)
@@ -51,4 +53,7 @@ function moviesHTML(search) {
 }
 
 renderMovies(localStorage.getItem("searchValue"))
+// .then(() => {
+//   loading.classList.remove("visible")
+// })
 searchResultEl.innerText = localStorage.getItem("searchValue")
